7ae9352b3dcf32f4df7d732d2ab6b5d9aef37185
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2023-01-25 17:47:11 -0600

    commands for Pivot Claw

--- FRCmainAuton2023/src/main/java/frc/robot/commands/RotateClawDown.java
+++ FRCmainAuton2023/src/main/java/frc/robot/commands/RotateClawDown.java
@@ -55,11 +55,13 @@ public class RotateClawDown extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_claw.rotateClawDown();
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_claw.rotateClawStop();
     }
 
     // Returns true when the command should end.
