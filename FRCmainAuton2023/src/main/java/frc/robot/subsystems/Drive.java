26d4a3f7856d86f6c5e1121370aad6fb3a1e6efb
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2023-02-20 13:21:30 -0600

    pushing starting of ramp up

--- FRCmainAuton2023/src/main/java/frc/robot/subsystems/Drive.java
+++ FRCmainAuton2023/src/main/java/frc/robot/subsystems/Drive.java
@@ -336,6 +336,47 @@ public class Drive extends SubsystemBase {
               rightSpeed /= maxMagnitude;
             }
 
+            //Madelena ramp up and down
+            double max = .9;
+            double rampUp = .01; // error allowed
+            double rampD = .01; // ramp down
+            double jsAxisY = power; // pull num from joystick
+            double sub = jsAxisY - oldAxisY;
+            
+            if (Math.abs(sub) > rampUp && sub > 0 && jsAxisY > .25) {
+            // test to see if it is going forward
+            tempAxisY = oldAxisY + rampUp;
+            } else if (Math.abs(sub) > rampD && sub < 0 && Math.abs(jsAxisY) > .25) { // test to see if it is going
+            //backwards
+            tempAxisY = oldAxisY - rampD;
+            } else {
+            tempAxisY = jsAxisY;
+            }
+            // test to see if value exceds max allowed
+            
+            if (tempAxisY < max * -1) {
+            tempAxisY = max * -1;
+            } else if (tempAxisY > max) {
+            tempAxisY = max;
+            }
+            finalAxisY = tempAxisY;
+            oldAxisY = finalAxisY;
+            
+           
+// end of Madelena's
+
+
+
+
+
+
+
+
+
+
+
+
+
         setPointLeft = leftSpeed * maxRPM;
         setPointRight = rightSpeed * maxRPM;
 
