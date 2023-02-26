8653521131ee27c1e8012aefb69986721366aa8e
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2023-02-26 14:49:15 -0600

    middle

--- FRCmainAuton2023/src/main/java/frc/robot/Constants.java
+++ FRCmainAuton2023/src/main/java/frc/robot/Constants.java
@@ -75,7 +75,7 @@ public class Constants {
 
         private double[][] placementValues = {
             {2,0.095,0.49,-40.28}, //floor
-            {250,9.6,.74,7.3}, // middle
+            {250,6.45,.74,7.3}, // middle
             {0,0,.73,5.24}, // pickup
             {8707,9.73,.62,-28.3}, // top
             {0,0,0.87,0}, // start
